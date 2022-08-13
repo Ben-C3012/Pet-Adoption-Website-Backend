ca21ff4ca78693324c1acf9771b4fd95784bba7d
unknown <ben.iisr.2004@gmail.com>
2022-08-13 15:30:47 +0300

    mongo first connection

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path: './.env'})
+
 const app = require('./app')
 
+const DB = process.env.DATABASE
+console.log(DB)
 
 
-console.log(process.env.HELLO)
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con => {
+    console.log(con)
+    console.log('DB Connection Successful!')
+})
 
-const port = 3003
+const port = 4320
 app.listen(port, () => {
     console.log('App Running On Port ' + port)
 })
