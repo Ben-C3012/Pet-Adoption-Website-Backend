6d8b09c7f023e8c9cf6e1782cf6b671e80a1a90b
unknown <ben.iisr.2004@gmail.com>
2022-08-14 22:35:56 +0300

    CRUD + CREATE USER

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,17 @@
 const User = require('./../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
 
-    try  {
-        const newUser = await User.create(req.body)
 
-        res.status.json({
-            status: 'success',
-            data: {
-                user: newUser
-            }
-        })
-    } catch(err) {
-        res.status(400).json({
-            status : 'fail', 
-            data : {
-                error : err.message
-            }
-        })
-    }
-   
-   next()
-}
+    const newUser = await User.create(req.body)
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user: newUser
+        }
+    })
+
+
+})
--- models/userModel.js
+++ models/userModel.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please Tel us Your name'],
-        unique : true,
     },
     email: {
         type: String,
